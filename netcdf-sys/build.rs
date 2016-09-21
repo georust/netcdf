2677a8782f203283b82644ffedb268dd4a4507ab
Yves MOISAN <you@example.com>
2016-09-21 22:02:59 +0200

    Renamed netcdf_v4.3.3.1.h to netcdf.h + Added include directory (build.rs / gcc)

--- netcdf-sys/build.rs
+++ netcdf-sys/build.rs
@@ -2,5 +2,8 @@ extern crate gcc;
 
 fn main() {
     // compile c wrapper to convert CPP constants into proper C types+values
-    gcc::compile_library("libncconst.a", &["src/ncconst.c"]);
+    gcc::Config::new()
+                .file("src/ncconst.c")
+                .include("src")
+                .compile("libncconst.a");
 }
