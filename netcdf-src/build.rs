d33cc5219d982eeac5def4275451640e4dad5b8f
Magnus Ulimoen <magnusu@met.no>
2024-07-29 11:47:44 +0200

    Remove old HDF5 options (unused)

--- netcdf-src/build.rs
+++ netcdf-src/build.rs
@@ -4,44 +4,11 @@ macro_rules! feature {
     };
 }
 
-fn get_hdf5_version() -> String {
-    let (major, minor, patch) = std::env::vars()
-        .filter_map(|(key, value)| {
-            key.strip_prefix("DEP_HDF5_VERSION_").map(|key| {
-                assert_eq!(value, "1");
-                let mut version = key.split('_');
-                let major: usize = version.next().unwrap().parse().unwrap();
-                let minor: usize = version.next().unwrap().parse().unwrap();
-                let patch: usize = version.next().unwrap().parse().unwrap();
-
-                (major, minor, patch)
-            })
-        })
-        .max()
-        .expect("Crate hdf5 should have emitted a hdf5 version");
-
-    format!("{major}.{minor}.{patch}")
-}
-
 fn main() {
     println!("cargo::rerun-if-changed=build.rs");
 
     let hdf5_incdir = std::env::var("DEP_HDF5_INCLUDE").unwrap();
-    let mut hdf5_lib = std::env::var("DEP_HDF5_LIBRARY").unwrap();
-    let mut hdf5_hl_lib = std::env::var("DEP_HDF5_HL_LIBRARY").unwrap();
-
     let hdf5_root = format!("{hdf5_incdir}/../");
-    #[cfg(unix)]
-    {
-        let mut hdf5_libdir = format!("{hdf5_root}/lib/");
-        if !std::path::Path::new(&hdf5_libdir).exists() {
-            hdf5_libdir = format!("{hdf5_root}/lib64/");
-        }
-        hdf5_lib = format!("{hdf5_libdir}/{hdf5_lib}.a");
-        hdf5_hl_lib = format!("{hdf5_libdir}/{hdf5_hl_lib}.a");
-    }
-
-    let hdf5_version = get_hdf5_version();
 
     let mut netcdf_config = cmake::Config::new("source");
     netcdf_config
