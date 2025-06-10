e3a981e39d27ed21d8049e893721858a9783989f
Magnus Ulimoen <magnusu@met.no>
2025-06-10 15:37:28 +0200

    Bump netcdf-c source

--- netcdf-src/src/lib.rs
+++ netcdf-src/src/lib.rs
@@ -1,6 +1,6 @@
 //! Dummy crate for building `netCDF` from source
 //!
-//! The current pinned version is 4.9.3
+//! The current pinned version is 4.9.3 + strlcpy patch
 
 #[cfg(feature = "dap")]
 extern crate link_cplusplus;
